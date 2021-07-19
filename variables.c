8d61eeeb1467afe6a8656e34e22331703b92c165
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-19 09:23:40 +0530

    Add files via upload
    intro of Variables in C

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{int a=8;
+
+//int b=7.8; // this is not recommended becaue b is not an integer;
+float b=7.8;
+char c= 'u';
+int d=9;
+printf("th value of a is %d \n", a); //by \n the new line will appear;
+printf("th value of b is %f \n", b);
+printf("the value of c is %c \n", c);
+printf(" the sum of a and d is %d \n", a+d);
+return 0;
+}
