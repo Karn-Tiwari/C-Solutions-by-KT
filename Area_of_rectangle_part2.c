9d9e4ed4440209341333d4a5fa6ef49dffb15798
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-20 19:00:28 +0530

    Add files via upload

---
+++ Area_of_rectangle_part2.c
@@ -0,0 +1,15 @@
+//Calculating area of rectangle by input user...
+#include<stdio.h>
+int main()
+{int length,breadth;
+
+printf("what is your length of the rectangle\n");
+scanf("%d", &length);
+
+printf("what is your breadth of the rectangle\n");
+scanf("%d",&breadth);
+
+printf("the area of the rectangle is %d",length*breadth);
+    
+    return 0;
+}
