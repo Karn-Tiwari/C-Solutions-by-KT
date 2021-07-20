188345aabaf43b29bbe4c19e4813fb4c0ae0611d
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-20 19:00:04 +0530

    Add files via upload

---
+++ Area_of_rectangle.c
@@ -0,0 +1,9 @@
+// Calculating area of Rectangle
+#include <stdio.h>
+int main()
+{
+    int length = 7, breadth = 9;
+    int area = length * breadth;
+    printf("the area of this rectangle is %d", area);
+    return 0;
+}
