75c6b581bbc8d2bc8a2084fceb2480a63988a5e9
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-21 08:42:53 +0530

    Add files via upload

---
+++ Area_of_circle.c
@@ -0,0 +1,11 @@
+//finding area and volume practice set question 2
+
+#include<stdio.h>
+int main()
+
+{int radius=3;
+float pi=3.14;int height=5;
+    /* code */printf("the area of circle is %f\n",pi*radius*radius);
+printf("the volume of the cylinder  is %f\n",pi*radius*radius*height);
+    return 0;
+}
