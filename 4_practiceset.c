aa6fa80b928e4524f29d7e20ca8bc8151840a286
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-24 23:57:12 +0530

    Add files via upload

---
+++ 4_practiceset.c
@@ -0,0 +1,11 @@
+//converting the celsius to fahrenheit by using formula;
+
+#include<stdio.h>
+int main()
+{
+    float celsius=67;
+    float far=(celsius*9/5) + 32;
+    printf("the value of celsius temperature in fahrenheit is %f", far);
+
+    return 0;
+}
---
+++ 5_practiceset.c
@@ -0,0 +1,10 @@
+//calculating simple interest of practiceset_1
+#include<stdio.h>
+int main()
+{
+    int principal=4000,rate=5,year=3;
+    int SI=(principal*rate*year)/100;
+    printf("the value of SI is %d",SI);
+
+    return 0;
+}
