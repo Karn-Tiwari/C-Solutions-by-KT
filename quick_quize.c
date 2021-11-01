b1902169768a8c13cd41e4d818c1f20a237da689
A. Amrutha Sri <81810889+AmruthaSri852@users.noreply.github.com>
2021-11-01 14:05:32 +0530

    Update quick_quize.c

--- quick_quize.c
+++ quick_quize.c
@@ -1,19 +1,38 @@
-//question : write a program to find a grade 
+//question : write a program to find a grade
 //of student givem his marks baesd on below ??
 #include<stdio.h>
 int main()
 {
-int garde;
-int marks;
-printf("enter your marks\n");
-scanf("%d",&marks);
-if(marks 90 - 100){
-    printf("your grade is A\n");
-}
-
-
-
-
-
+    int marks;
+    printf("Enter your marks(0-100): ");
+    scanf("%d",&marks);
+    if(marks<0 || marks>100)
+    {
+        printf("Plz enter your marks in between [0,100]\n");
+    }
+    else if(marks>=91 && marks<100)
+    {
+        printf("Grade O - Outstanding performance!");
+    }
+    else if(marks>=81 && marks<91)
+    {
+        printf("Grade A");
+    }
+    else if(marks>=71 && marks<81)
+    {
+        printf("Grade B");
+    }
+    else if(marks>=61 && marks<71)
+    {
+        printf("Grade C");
+    }
+    else if(marks>=51 && marks<61)
+    {
+        printf("Grade D");
+    }
+    else
+    {
+        printf("Grade F - Failed...");
+    }
     return 0;
 }
